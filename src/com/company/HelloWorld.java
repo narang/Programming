73cff44bac49907934eaef45ec7eb01f2e103e67
Sourabh Kumar <sourabh.kumar@tesco.com>
2021-08-11 14:02:01 +0530

    HelloWorld code is added

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class HelloWorld {
+
+    private static void printHello() {
+        System.out.println("Hello World");
+    }
+
+    public static void run() {
+        printHello();
+    }
+}
